9d74e79a2b64944041b1faa9f4bd2a5a7b414d3e
moses-ramoeletsi <mramoeletsi14@gmail.com>
2025-03-25 12:59:15 +0200

    made changes

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -16,7 +16,7 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn("p-3 pointer-events-auto", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
@@ -52,9 +52,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        iconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        iconRight: () => <ChevronRight className="h-4 w-4" />,
+      }as any}
       {...props}
     />
   );
--- client/src/components/users/UserTable.tsx
+++ client/src/components/users/UserTable.tsx
@@ -1,6 +1,5 @@
 
 import React from "react";
-import { UserRole } from "../../context/AuthContext";
 import { UserData } from "../../types/user";
 import { Badge } from "../../components/ui/badge";
 import { Button } from "../../components/ui/button";
